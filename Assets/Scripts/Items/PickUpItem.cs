7229ac1cda88162d12dab76378cb8569cb3e3e1b
BorysMandryk <borys.mandryk@gmail.com>
2024-04-21 19:09:02 +0300

    Add items pickup

---
+++ Assets/Scripts/Items/PickUpItem.cs
@@ -0,0 +1,27 @@
+using Player;
+using UnityEngine;
+
+namespace Items
+{
+    public class PickUpItem : MonoBehaviour
+    {
+        [SerializeField] private int _mass = 1;
+
+        private bool _pickedUp = false;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (_pickedUp)
+            {
+                return;
+            }
+
+            if (collision.CompareTag("Player"))
+            {
+                var playerInventory = collision.GetComponent<PlayerInventory>();
+                playerInventory.Mass++;
+                _pickedUp = true;
+            }
+        }
+    }
+}
